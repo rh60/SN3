5819f8e7997911ece448168f6e98d75a20c1ae3c
rh60 <32161865+rh60@users.noreply.github.com>
2018-12-03 17:07:30 +0100

    Add files via upload

--- dim2/fem.jl
+++ dim2/fem.jl
@@ -19,13 +19,14 @@ struct Assembly
 end
 
 @inline function CalculateContributionOnTriangle!(a::Assembly,k::Int)
-    t=a.msh.tri[k,1:3]
+    t=a.msh.tri[k,:]
     x=a.msh.x[t];y=a.msh.y[t];
-    qx=affinity(x,a.Q.Points.x)
-    qy=affinity(x,a.Q.Points.y)
+    qx=affinity(x,a.Q.Points.x,a.Q.Points.y)
+    qy=affinity(y,a.Q.Points.x,a.Q.Points.y)
     J,invJT=LinearTranform(x,y)
     l=length(a.L)
     p=a.bvp.p.(qx,qy)
+    f=a.bvp.f.(qx,qy)
     for i=1:l
         for j=1:l
             push!(a.I,t[i]); push!(a.J,t[j])
@@ -34,18 +35,19 @@ end
             g=gi[1,:].*gj[1,:]+gi[2,:].*gj[2,:]
             push!(a.V,intSum(a.Q,p.*g)*J)
         end
-        a.b[t[i]] += intSum(a.Q,a.bvp.f.(qx,qy).*a.L[i])*J
+        a.b[t[i]] += intSum(a.Q,f.*a.L[i])*J
     end
 end
 
 #function FEM
-msh=Mesh(Rectangle(0,1,0,1),10,10)
-E=Lagrange(1)
-f(x,y)=2*f1(x,y)
+msh=Mesh(Rectangle(0,1,0,1),30,30)
+
+E=Lagrange(2)
+f(x,y)=-2*y*(y-1)-2*x*(x-1)
 vbc=[BoundaryCondition(0,f0) for i=1:4]
 bvp=BoundaryValueProblem(f,vbc)
 
-a=Assembly(msh,bvp,E,4)
+a=Assembly(LagrangeMesh(msh,2),bvp,E,4)
 nt=size(a.msh.tri,1)
 for k=1:nt
     CalculateContributionOnTriangle!(a,k)
@@ -53,15 +55,19 @@ end
 A=sparse(a.I,a.J,a.V)
 dirt=Int[]
 for ib=1:4
-    global dirt = [dirt; a.msh.boundary[ib][:,1]]
+    global dirt = [dirt; a.msh.boundary[ib][:,1]; a.msh.boundary[ib][:,3] ]
 end
 A[dirt,:].=0
 for i in dirt
     A[i,i]=1
 end
 a.b[dirt].=0
-U=A\a.b
 
-write_matfile("data/poisson.mat",msh=a.msh,U=U)
+U=A\a.b
+u(x,y)=x*(x-1)*y*(y-1)
+print("norm(u-U)="); display(norm(u.(a.msh.x,a.msh.y)-U,Inf))
+
+write_matfile("data/poisson.mat",msh=Refine(msh,2),U=U)
+
 
 #end
--- dim2/showsol.m
+++ dim2/showsol.m
@@ -1,7 +1,12 @@
 clear;clc;close;
 load('../data/poisson.mat');
 tri=double(msh.tri(:,1:3));
-
+x=msh.x;y=msh.y;
 hold off
-trisurf(tri,msh.x,msh.y,U);
-
+trisurf(tri,x,y,U);
+fu=@(x,y) x.*(x-1).*y.*(y-1);
+u=fu(x,y);
+figure
+trisurf(tri,x,y,u-U);
+[m,i]=max(u-U)
+[x(i),y(i)]
