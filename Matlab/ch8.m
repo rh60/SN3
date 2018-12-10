81fa9000702eb9c6e986597b164a499f80050316
rh60 <32161865+rh60@users.noreply.github.com>
2018-12-10 16:23:39 +0100

    Add files via upload

--- Matlab/ch8.m
+++ Matlab/ch8.m
@@ -1,1 +1,8 @@
-Chap8_CalculateExampleFem(100,100);
+clear;clc;close;
+[x, y, tri, U]=Chap8_CalculateExampleFem(5,5);
+u=x.^2+2*y.^3;
+trisurf(tri,x,y,U);
+figure;
+er=u-U;
+trisurf(tri,x,y,er);
+max(abs(er))
